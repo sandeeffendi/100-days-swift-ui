faf8f8d8e182c57f6a55337b4b41687aa66ed072
Sunday Morning <sande.effendi@gmail.com>
2026-03-08 22:46:04 +0700

    initial commit

---
+++ Day-1.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Cocoa
+
+var greeting = "hello from swift"
+
+greeting = "hello playgroung"
+greeting = "hello word"
+
+greeting = "hello world"
+
+greeting = "hellow"
+
+
+greeting = "hellow wolrd"
+greeting = "Hello world"
