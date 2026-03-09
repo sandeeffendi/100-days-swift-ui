ae6db992177a340642489d090465d286f8a16d19
Sande Effendi <sande.effendi@gmail.com>
2026-03-09 14:57:59 +0700

    how to write string on swift

--- Day-1.playground/Contents.swift
+++ Day-1.playground/Contents.swift
@@ -1,14 +1,57 @@
 import Cocoa
 
-var greeting = "hello from swift"
+// this is a dynamic variabel
+var managerName = "Daphne"
+var employeeName = "John"
 
-greeting = "hello playgroung"
-greeting = "hello word"
+// this is a constant variabel
+let firstName = "Sande"
+let lastName = "Effendi"
 
-greeting = "hello world"
+// this is how to print value
+print(managerName)
+print(employeeName)
 
-greeting = "hellow"
+// this is how to print string interpolation
+print("\(firstName) \(lastName)")
 
 
-greeting = "hellow wolrd"
-greeting = "Hello world"
+// how to create string
+var singleLineString = "ini adalah string single line"
+var multiLineString = """
+ini adalah
+multi line
+string
+"""
+ 
+var singleLineStringLagi = "ini adalah string single line lagi"
+
+print(singleLineString)
+print(multiLineString)
+
+print(multiLineString.count)
+
+
+print(multiLineString.uppercased())
+print(singleLineString.uppercased())
+
+// how to store whole number
+
+let score = 1_000_000
+
+// number operators
+let decremement = score - 1
+let increment = score + 2
+let divide = score / 2
+let multiple = score * 2
+
+print(decremement)
+
+print(increment)
+print(decremement)
+print(increment.isMultiple(of: 3))
+print(divide)
+print(multiple)
+print(decremement.isMultiple(of: 2))
+
+print(increment)
